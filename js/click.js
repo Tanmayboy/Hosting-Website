4b9e941c47e2f944cbab08be4f976ea1637e5b6a
Tanmay <100485745+Tanmayboy@users.noreply.github.com>
2024-09-04 23:51:08 +0530

    Js

--- js/click.js
+++ js/click.js
@@ -1,1 +1,82 @@
+class ClickSpark extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
+  connectedCallback() {
+    this.shadowRoot.innerHTML = this.createSpark();
+    this.svg = this.shadowRoot.querySelector("svg");
+    this._parent = this.parentNode;
+    this._parent.addEventListener("click", this);
+  }
+
+  disconnectedCallback() {
+    this._parent.removeEventListener("click", this);
+    delete this._parent;
+  }
+
+  handleEvent(e) {
+    this.setSparkPosition(e);
+    this.animateSpark();
+  }
+
+  animateSpark() {
+    let sparks = [...this.svg.children];
+    let size = parseInt(sparks[0].getAttribute("y1"));
+    let offset = size / 2 + "px";
+
+    let keyframes = (i) => {
+      let deg = `calc(${i} * (360deg / ${sparks.length}))`;
+
+      return [
+        {
+          strokeDashoffset: size * 3,
+          transform: `rotate(${deg}) translateY(${offset})`
+        },
+        {
+          strokeDashoffset: size,
+          transform: `rotate(${deg}) translateY(0)`
+        }
+      ];
+    };
+
+    let options = {
+      duration: 660,
+      easing: "cubic-bezier(0.25, 1, 0.5, 1)",
+      fill: "forwards"
+    };
+
+    sparks.forEach((spark, i) => spark.animate(keyframes(i), options));
+  }
+
+  setSparkPosition(e) {
+    this.style.left = e.pageX - this.clientWidth / 2 + "px";
+    this.style.top = e.pageY - this.clientHeight / 2 + "px";
+  }
+
+  createSpark() {
+    return `
+      <style>
+        :host {
+          position: absolute;
+          pointer-events: none;
+        }
+      </style>
+      <svg width="30" height="30" viewBox="0 0 100 100" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" stroke="var(--click-spark-color, currentcolor)" transform="rotate(-20)">
+        ${Array.from(
+          { length: 8 },
+          (_) =>
+            `<line x1="50" y1="30" x2="50" y2="4" stroke-dasharray="30" stroke-dashoffset="30" style="transform-origin: center" />`
+        ).join("")}
+      </svg>
+    `;
+  }
+}
+
+customElements.define("click-spark", ClickSpark);
+const spark = document.querySelector("click-spark");
+const colorPicker = document.getElementById("click-spark-color");
+colorPicker.addEventListener("change", (e) => {
+  spark.style.setProperty("--click-spark-color", e.target.value);
+});
---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2024, 9, 2, 1)}); // year, month, date, hour
+        });
+});		
+
